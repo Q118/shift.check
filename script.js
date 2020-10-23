7ac2b3bc426d6343da1e79ae31b5f5086dc01b25
Q118 <shelbyfish91@gmail.com>
2020-10-22 17:00:18 -0700

    setting up functionality of shift

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("hi");
+
+/*
+create function to track of the id of the last checked box and 
+when when another checkbox is checked they use the shiftKey event 
+attribute to see if shift was held while clicking the checkbox. 
+If so they set the checked property of each checkbox in between the two to true.
+To determine when a box is checked they probably use an onclick event on the checkboxes */
+
+function shiftGrab() {
+    
+}
